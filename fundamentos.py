97f8ca63c4ff1f42a247163860c94a2a4301bf08
Eliana Gamboa <1206velandia.g@gmail.com>
2026-04-29 12:29:03 -0500

    calculadora

--- fundamentos.py
+++ fundamentos.py
@@ -1,3 +1,64 @@
+from math import e
+from operator import truediv
+
+
 print ("hello world")
+
+
+mi_avriable = "hola mundo"
+
+
+calmeCase = "SENA"
+aprendizSena = "SENA"
+
+PascalCase = "SENA"
+aprendizCase = "SENA"
+
+snake_case = "SENA"
+aprendiz_Case = "SENA"
+
+print(calmeCase)
+print(PascalCase)
+print(snake_case)
+
+#estrutura de antiguo 2.0
+
+nombre = "gato"
+apellido = "pris"
+edad = 99
+altura = 3.40
+activo = truediv
+correo = "jsyajdgfefhf@sena.edu.co"
+telefono = "3001142090"
+cedula = "345677654"
+telefono_int = int(telefono)
+
+print(type(nombre)),
+print(type(apellido)),apellido
+print(type(edad)),edad
+print(type(altura)),altura
+print(type(activo)),activo
+print(type(correo)),correo
+print(type(telefono_int)),telefono
+print(type(telefono),telefono_int)
+
+print(type(nombre)),
+print(type(apellido)),apellido
+print(type(edad)),edad
+print(type(altura)),altura
+print(type(activo)),activo
+print(type(correo)),correo
+print(type(telefono_int)),telefono
+print(type(telefono),telefono_int)
+
+# i         = 0
+# while i < 10:             FBFG BV YI
+
+
+if True:
+    print("camilo")   
+    for i in range(80):
+        print("camilo")
+
 
 
---
+++ operadores.py
@@ -0,0 +1,26 @@
+
+
+a = float(input("ingrese un numero: "))
+b = float(input("ingrese otro numero: "))
+tipo_de_operacion = input("ingrese el tipo de operacion que desea realizar: 1.suma 2.resta 3.multiplicacion 4.division 5.modulo 6.potencia: ")
+
+
+suma = a + b
+resta = a - b
+multiplicacion = a *  b
+division = a / b
+modulo = a % b
+potencia = a ** b
+ 
+if tipo_de_operacion == "1":
+    print("la suma es: ",suma)
+elif tipo_de_operacion == "2":
+    print("la resta es: ",resta)
+elif tipo_de_operacion == "3":
+    print("la multiplicacion es: ",multiplicacion)
+elif tipo_de_operacion == "4":
+    print("la division es: ",division)
+elif tipo_de_operacion == "5":
+    print("el modulo es: ",modulo)
+elif tipo_de_operacion == "6":
+    print("la potencia es: ",potencia)
