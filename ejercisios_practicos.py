3e73cf437e34ab07eb7c995d667c2dcebfc813af
Eliana Gamboa <1206velandia.g@gmail.com>
2026-05-08 12:11:11 -0500

    trabajo

---
+++ ejercisios_practicos.py
@@ -0,0 +1,56 @@
+# ejercisio 1 
+
+from socket import 
+
+
+nombre = "juan"
+producto = 20000
+promedio_asignado=8.5
+
+print("Hola, mi nombre es ", nombre, "y el producto cuesta ", producto, "y el promedio asignado es ", promedio_asignado)
+
+# ejercisio 2
+
+variable_1_entera = int(input("ingresa un numero entero: "))
+variable_2_flotante = int(input("ingresa otro numero entero: "))
+varible_float = float(input("ingresa un numero decimal: "))
+variable_1_string = input("ingresa un texto: ")
+variable_2_string = input("ingresa otro texto: ")
+
+suma_numeros =float (variable_1_entera + variable_2_flotante + varible_float) 
+
+
+# ejercisio 3
+
+base = 5
+exponente =3
+resultado = base ** exponente
+print(F"El resultado de {base} elevado a la {exponente} es {resultado}")
+
+
+
+# ejercisio 4
+
+import math
+
+numero = [2, 7 , 10, 15, 20]
+for numero in numero :
+    raiz = math.sqrt(numero)
+    print(F"La raiz cuadrada de {numero} es {raiz}")
+
+
+
+# ejercisio 5
+nombre = input("ingresa el nombre del estudiante: ")
+
+nota_1 = float(input("ingresa la nota 1: "))    
+nota_2 = float(input("ingresa la nota 2: "))
+nota_3 = float(input("ingresa la nota 3: "))
+nota_4 = float(input("ingresa la nota 4: "))
+nota_5 = float(input("ingresa la nota 5: "))
+
+promedio = (nota_1 + nota_2 + nota_3 + nota_4 + nota_5) / 5
+
+
+#ejercisio 6
+
